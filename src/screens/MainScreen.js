79cb80162f54ca5428d9880c14cb8d3659b1dbd3
sashabull66 <sashabull9042@gmail.com>
2021-08-23 12:40:26 +0300

    add img for main screen when no todo

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,22 +1,42 @@
 import React from "react";
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList, Image} from 'react-native';
 import AddToDo from "../components/AddToDo.js";
 import Todo from "../components/Todo.js";
 
 export const MainScreen = ({addTodo, removeTodo, todos, onOpen}) => {
-    return (
-        <View style={styles.MainScreen}>
-            <AddToDo onSubmit={addTodo}/>
+    let content = (
+        <FlatList
+            keyExtractor={item => item.id.toString()}
+            data={todos}
+            renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} openCurrentTodo={onOpen}/>}
+        />
+    )
 
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={todos}
-                renderItem={({item}) => <Todo todo={item} onRemove={removeTodo} openCurrentTodo={onOpen}/>
-                }/>
+    if (todos.length === 0) {
+        content = (
+            <View style={styles.imgWrapper}>
+                <Image style={styles.img} source={require('../../assets/empty.png')}/>
+            </View>
+        )
+    }
+    return (
+        <View>
+            <AddToDo onSubmit={addTodo}/>
+            {content}
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    MainScreen: {}
+    imgWrapper: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        height: 300,
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
 })
